5fcd229731e3b78ee8c05a9fce80c00d04621139
BKUKB <3054360154@qq.com>
2023-06-26 16:36:34 +0800

    服务器端DP

---
+++ framework/homo/aggregator/__init__.py
@@ -0,0 +1,4 @@
+from federatedml.framework.homo.aggregator.secure_aggregator import SecureAggregatorClient, SecureAggregatorServer
+from federatedml.framework.homo.aggregator.dp_secure_aggregator import DPSecureAggregatorClient, DPSecureAggregatorServer
+
+__all__ = ['SecureAggregatorClient', 'SecureAggregatorServer','DPSecureAggregatorClient','DPSecureAggregatorServer']
